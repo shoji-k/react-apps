4ab040ec820f2db2e91944858e6c330b7350d81f
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2022-02-01 11:35:39 +0900

    prepare tailwind base

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Dice from "./components/pages/Dice";
 import Top from "./components/pages/Top";
 
 function App() {
   return (
-    <>
-      <header>
-        <h1>Freks sample apps</h1>
+    <div className="h-screen flex flex-col px-4 py-2">
+      <header className="grow-0 pb-2">
+        <h1>
+          <Link to="/">Freks sample apps</Link>
+        </h1>
       </header>
-      <main>
+      <main className="grow pb-2">
         <Routes>
           <Route path="/" element={<Top />} />
           <Route path="dice" element={<Dice />} />
           <Route path="*" element={<p>There's nothing here!</p>} />
         </Routes>
       </main>
-    </>
+      <footer className="grow-0">
+        <a
+          href="https://freks.jp"
+          className="no-underline hover:underline"
+        >
+          <small>freks</small>
+        </a>
+      </footer>
+    </div>
   );
 }
 
