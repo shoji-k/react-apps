627c187b20fa180d3b5e17541d283861174845cc
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2022-02-02 17:43:09 +0900

    split DiceCounter

--- src/components/organisms/Dice.tsx
+++ src/components/organisms/Dice.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { LoadingIcon } from "../atoms/LoadingIcon";
+import { DiceCounter } from "./Dice/Counter";
 
 type CounterList = [number, number, number, number, number, number];
 
@@ -48,31 +49,7 @@ export function Dice() {
         </div>
       </div>
       <div className="pb-2">
-        <div className="pb-2">Counter</div>
-        <div>
-          <table className="table-auto border-collapse border border-slate-400">
-            <thead>
-              <tr>
-                <th className="border border-slate-300 px-2">1</th>
-                <th className="border border-slate-300 px-2">2</th>
-                <th className="border border-slate-300 px-2">3</th>
-                <th className="border border-slate-300 px-2">4</th>
-                <th className="border border-slate-300 px-2">5</th>
-                <th className="border border-slate-300 px-2">6</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="border border-slate-300 px-2">{countList[0]}</td>
-                <td className="border border-slate-300 px-2">{countList[1]}</td>
-                <td className="border border-slate-300 px-2">{countList[2]}</td>
-                <td className="border border-slate-300 px-2">{countList[3]}</td>
-                <td className="border border-slate-300 px-2">{countList[4]}</td>
-                <td className="border border-slate-300 px-2">{countList[5]}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <DiceCounter countList={countList} />
       </div>
       <div className="pb-2">
         <div className="pb-2">Log</div>
---
+++ src/components/organisms/Dice/Counter.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+type CounterList = [number, number, number, number, number, number];
+
+type Props = {
+  countList: CounterList;
+};
+
+export const DiceCounter: React.FC<Props> = ({ countList }) => {
+  return (
+    <>
+      <div className="pb-2">Counter</div>
+      <div>
+        <table className="table-auto border-collapse border border-slate-400">
+          <thead>
+            <tr>
+              <th className="border border-slate-300 px-2">1</th>
+              <th className="border border-slate-300 px-2">2</th>
+              <th className="border border-slate-300 px-2">3</th>
+              <th className="border border-slate-300 px-2">4</th>
+              <th className="border border-slate-300 px-2">5</th>
+              <th className="border border-slate-300 px-2">6</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border border-slate-300 px-2">{countList[0]}</td>
+              <td className="border border-slate-300 px-2">{countList[1]}</td>
+              <td className="border border-slate-300 px-2">{countList[2]}</td>
+              <td className="border border-slate-300 px-2">{countList[3]}</td>
+              <td className="border border-slate-300 px-2">{countList[4]}</td>
+              <td className="border border-slate-300 px-2">{countList[5]}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
