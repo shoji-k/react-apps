34c7ead3aa70682b174db52c75db48cc645eaf95
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2022-02-07 17:48:30 +0900

    change title

--- src/components/pages/dice.tsx
+++ src/components/pages/dice.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Dice } from "../organisms/Dice";
 
 export default function DicePage() {
+  useEffect(() => {
+    document.title = "Dice | Freks app";
+  }, []);
+
   return (
     <>
       <h2 className="pb-2 text-lg font-semibold">Dice</h2>
