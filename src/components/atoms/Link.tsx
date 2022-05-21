25d33d2fb90bbf3bc025c468dc143e66d50a9dfe
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2022-05-21 12:31:10 +0900

    bump up versions and fix

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Link as ReactRouterLink } from "react-router-dom";
 
 type Props = {
+  children: React.ReactNode;
   to: string;
 };
 
-export const Link: React.FC<Props> = ({ to, children }) => (
+export const Link: React.FC<Props> = ({ children, to }) => (
   <ReactRouterLink
     to={to}
     className="no-underline hover:underline text-teal-500"
