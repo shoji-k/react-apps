b001429ca5b6a3d257b9d3d873133d6cef7a2a24
shoji kobayashi <shoji.kobayashi.0125@gmail.com>
2022-11-11 14:42:31 +0900

    add spyOn tests

--- src/components/organisms/Api.test.tsx
+++ src/components/organisms/Api.test.tsx
@@ -5,8 +5,20 @@ import { Api } from "./Api";
 jest.mock("axios");
 
 test("render Api", async () => {
-  const promise = Promise.resolve({ data: { origin: "192.0.0.1" } });
+  const expected = { data: { origin: "192.0.0.1" } };
+  const promise = Promise.resolve(expected);
   (axios.get as any).mockImplementationOnce(() => promise);
+
+  render(<Api />);
+
+  await waitFor(() =>
+    expect(screen.getByText("ip: 192.0.0.1")).toBeInTheDocument()
+  );
+});
+
+test("render Api using spyOn", async () => {
+  const expected = { data: { origin: "192.0.0.1" } };
+  jest.spyOn(axios, 'get').mockResolvedValue(expected);
 
   render(<Api />);
   await waitFor(() =>
