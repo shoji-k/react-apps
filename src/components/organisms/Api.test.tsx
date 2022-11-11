346cbdd8fd157611d317ffade19bc92cfcb69574
shoji kobayashi <shoji.kobayashi.0125@gmail.com>
2022-11-11 13:29:01 +0900

    add Api

--- src/components/organisms/Api.test.tsx
+++ src/components/organisms/Api.test.tsx
@@ -1,8 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import axios from "axios";
 import { Api } from "./Api";
 
-test("render Api", () => {
+jest.mock("axios");
+
+test("render Api", async () => {
+  const promise = Promise.resolve({ data: { origin: "192.0.0.1" } });
+  (axios.get as any).mockImplementationOnce(() => promise);
+
   render(<Api />);
-  screen.debug()
-  expect(screen.getByText("ip:")).toBeInTheDocument();
+  await waitFor(() =>
+    expect(screen.getByText("ip: 192.0.0.1")).toBeInTheDocument()
+  );
 });
--- src/components/organisms/Api.tsx
+++ src/components/organisms/Api.tsx
@@ -1,14 +1,25 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 export function Api() {
   const [ip, setIp] = useState("");
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    setIp("");
+    (async function () {
+      try {
+        const result = await axios.get("http://httpbin.org/ip");
+        setIp(result.data.origin);
+      } catch (error) {
+        console.log(error);
+        setError("error");
+      }
+    })();
   }, []);
 
   return (
     <div>
+      {error && <div>{error}</div>}
       <div>ip: {ip}</div>
     </div>
   );
